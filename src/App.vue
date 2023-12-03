e90e04eef25e9fd5c534e61efb475f35a146c77e
SeregeKEK <kurnakov2012@mail.ru>
2023-12-04 00:29:14 +0300

    vue-cli-mark2

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
   <div id="app">
     <BlogDetails/>
+    <BlogCards/>
+    <PageFooter/>
+    <PageHeader/>
   </div>
 </template>
 
 <script>
 import BlogDetails from './components/BlogDetails.vue'
+import BlogCards from './components/BlogCards.vue'
+import PageFooter from './components/PageFooter.vue'
+import PageHeader from './components/PageHeader.vue'
 export default {
   name: 'App',
   components: {
-    BlogDetails
+    BlogDetails,
+    BlogCards,
+    PageFooter,
+    PageHeader
   }
 }
 </script>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="header">
+        <a href="#" class="header__logo">
+            <img class="header__logo" src="img/LogoInterno.svg" alt="logo">
+        </a>
+        <nav class="main-nav">
+            <a class="main-nav__link" href="#">Home</a>
+            <a class="main-nav__link" href="#">Project</a>
+            <a class="main-nav__link" href="#">Blog</a>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'PageHeader'
+}
+</script>
